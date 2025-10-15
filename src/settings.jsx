4443a0c58ff2548ea5104d2e0e7eaf70ebd97137
neerob347 <rxneerob@gmail.com>
2025-10-15 17:06:33 +0600

    added satting.jsx

---
+++ src/settings.jsx
@@ -0,0 +1,6 @@
+export const AppName="AppsStore BD";
+export const GitHubLink="https://github.com/neerob347";
+export const PlayStoreLink="https://play.google.com/store/apps?hl=en";
+export const AppStoreLink="https://www.apple.com/app-store/"
+
+
